363cf7e6cfee2e682ecab4f796957fa5761c0be7
Cesar-Ch <chafloque1530@gmail.com>
2023-08-12 23:09:17 -0500

    📝 constructor added

--- Day06class/Second.java
+++ Day06class/Second.java
@@ -1,13 +1,29 @@
 package Day06class;
 
 class Second {
+
+    int modelYear;
+    String modelName;
+
+    Second(int year, String name) {
+        this.modelYear = year;
+        this.modelName = name;
+    }
+
+    void myDescriptionCar() {
+        System.out.println("My car is a " + this.modelYear + " " + this.modelName);
+    }
+
     public static void main(String[] args) {
         Main myObj = new Main();
         myObj.x = 40;
         System.out.println(myObj.x);
-        //myStaticMethod(); // Call the static method
+        // myStaticMethod(); // Call the static method
         // myPublicMethod(); This would compile an error
 
         myObj.myPublicMethod(); // Call the public method on the object
+
+        Second myCar = new Second(1969, "Mustang");
+        myCar.myDescriptionCar();
     }
 }
