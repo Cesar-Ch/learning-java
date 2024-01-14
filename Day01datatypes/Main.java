97c432c70bf98f3809585625e8da85d711a691fb
Saavedra <danixoganador@hotmail.com>
2024-01-13 19:22:42 -0500

    rename name

--- Day01datatypes/Main.java
+++ Day01datatypes/Main.java
@@ -2,7 +2,7 @@ package Day01datatypes;
 
 public class Main {
     public static void main(String[] args) {
-        String name = "Daniel"; // almacena texto. Los valores de cadena están entre comillas dobles
+        String name = "cholo"; // almacena texto. Los valores de cadena están entre comillas dobles
         System.out.println(name);
 
         byte myByte = 127; // almacena nÚmeros enteros de 8 bits mayores a -128 y menores a 127
